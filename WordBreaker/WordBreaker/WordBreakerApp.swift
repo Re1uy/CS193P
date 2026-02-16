d6728c4c9c512303b64f1ce8eda8f695e3576ccb
Re1uy <2425035523@qq.com>
2026-02-16 13:07:29 -0500

    1

---
+++ WordBreaker/WordBreaker/WordBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordBreakerApp.swift
+//  WordBreaker
+//
+//  Created by 千秋 on 2026/2/16.
+//
+
+import SwiftUI
+
+@main
+struct WordBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
