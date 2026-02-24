1ddee27517c19c5cb38dfcc276299b0d7a4773e4
Re1uy <2425035523@qq.com>
2026-02-24 18:09:08 -0500

    Finish A5

---
+++ WordBreaker/WordBreaker/UI/Flitter.swift
@@ -0,0 +1,18 @@
+//
+//  Flitter.swift
+//  WordBreaker
+//
+//  Created by 千秋 on 2026/2/24.
+//
+
+import SwiftUI
+
+struct Flitter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Flitter()
+}
---
+++ WordBreaker/WordBreaker/UI/GameList.swift
@@ -0,0 +1,18 @@
+//
+//  GameList.swift
+//  WordBreaker
+//
+//  Created by 千秋 on 2026/2/24.
+//
+
+import SwiftUI
+
+struct GameList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameList()
+}
